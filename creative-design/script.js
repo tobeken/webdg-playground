b892cae7ad851fd4606c8472c2ab2abdbd8cca5b
tobioka <tobioka@ip-10-241-3-74.ap-northeast-1.compute.internal>
2023-11-23 18:19:20 +0900

    dreative design

---
+++ creative-design/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.story-btn').forEach((btn)=> {
+    btn.addEventListener("click",()=> {
+       btn.classList.toggle("change")
+       btn.nextElementSibling.classList.toggle("change")
+    })
+
+})
+
